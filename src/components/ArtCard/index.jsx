8434f332a96481466431228fc74883b8fc852334
Daphné Augier <daphne.augier@gmail.com>
2025-02-26 22:15:18 +0100

    title can't get focus

--- src/components/ArtCard/index.jsx
+++ src/components/ArtCard/index.jsx
@@ -3,30 +3,26 @@ import ModalImage from "react-modal-image";
 
 import "./index.css";
 
-function ArtCard({ title, description, picture }) {
+function ArtCard({ description, picture }) {
   return (
     <div className="card">
-    <ModalImage
-      className="modal-image"
-      small={`/assets/images/art/${picture}`}
-      large={`/assets/images/artbig/${picture}`}
-      hideDownload={true}
-      hideZoom={true}
-      alt={title}
-    />
-      <h3 className="overlay-text">{description}</h3>
-      </div>
+      <ModalImage
+        className="modal-image"
+        small={`/assets/images/art/${picture}`}
+        large={`/assets/images/artbig/${picture}`}
+        title={description}
+        hideDownload={true}
+        hideZoom={true}
+        alt={description}
+      />
+        <h3 inert="true" className="overlay-text">{description}</h3>
+    </div>
   );
 }
 
 ArtCard.propTypes = {
-  title: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   picture: PropTypes.string.isRequired,
 };
-  /*
-            <img src={`/assets/images/art/${picture}`} alt={title} />
-             <caption className='caption'>{title}</caption>
-            <p className='description' dangerouslySetInnerHTML={{__html: description}}></p> */
 
 export default ArtCard;
