09e16e2a55da062f4aa3052753a22f1bdcb690c0
Daphné Augier <daphne.augier@gmail.com>
2025-02-26 21:22:14 +0100

    Centered Menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,26 @@
-import './index.css'
-import { faFilePdf } from '@fortawesome/free-solid-svg-icons'
-import Menu from '../Menu'
+import "./index.css";
+import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
+import Menu from "../Menu";
 
 function Header() {
   return (
-    <header>
+    <>
+      <header>
+        <nav className="main-nav">
+          <Menu className="main-menu" />
+        </nav>
+      </header>
       <nav className="download-nav">
-      <a href="/pdf/CV_DaphneAugier.pdf" className="header-button" target='_blank'>
-      Download CV
-      </a>
+        <a
+          href="/pdf/CV_DaphneAugier.pdf"
+          className="header-button"
+          target="_blank"
+        >
+          Download CV
+        </a>
       </nav>
-      <nav className='main-nav'>
-        <Menu  className="main-menu"/>
-      </nav>
-    </header>
-
-  )
+    </>
+  );
 }
 
-export default Header
+export default Header;
