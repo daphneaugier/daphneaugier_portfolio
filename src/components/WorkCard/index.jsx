c28f1e7c5acd227105494642e8396705f4d96580
Daphné Augier <daphne.augier@gmail.com>
2025-03-02 18:59:05 +0100

    Responsive improvements

--- src/components/WorkCard/index.jsx
+++ src/components/WorkCard/index.jsx
@@ -11,10 +11,10 @@ function WorkCard({ work }) {
         className="work-img"
         data-aos="slide-left"
       />
-      <div className="work-text" data-aos="zoom-in">
+      <div className="workcard-text" data-aos="zoom-in">
         <div className="work-text-row">
           <h3 className="work-id">{work.id}</h3>
-          <h3 className="work-title">{work.title}</h3>
+          <h3 className="workcard-title">{work.title}</h3>
           <h4 className="work-text-year">{work.year}</h4>
           <div className="work-text-2col">
             <div>
