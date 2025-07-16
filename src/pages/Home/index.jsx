b5166f80884b8b842311af12ed12a231fff59c34
Daphné Augier <daphne.augier@gmail.com>
2025-07-16 23:11:35 +0200

    round scrolling button link to works

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,28 +1,52 @@
-import { useEffect } from 'react'
+import { useEffect } from "react";
 
-import Hero from '../../components/Hero'
-import WorkGallery from '../../components/WorkGallery'
-import ButtonArt from '../../components/MyButton'
-
+import Hero from "../../components/Hero";
+import WorkGallery from "../../components/WorkGallery";
+import ButtonArt from "../../components/MyButton";
+import ReactCurvedText from "react-curved-text";
 //Import Data
-import works from '../../data/works.json'
-import video from '../../assets/videos/daphne-desk.mp4'
-
+import works from "../../data/works.json";
+import video from "../../assets/videos/daphne-desk.mp4";
+import "./index.css";
+import { Link } from "react-router-dom";
 function Home() {
+  const text = "Scroll down to read";
+  const fontSize = 32;
+  const textPathFill = null;
 
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, []);
-  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <div>
+      <div className="hero">
       <Hero video={video} />
-      <WorkGallery works={(works)} />
-      <br/>
-      <br/>
-      <ButtonArt link="art" label="Art!" size="large" />
+      <a href="#works" className="curved-text-link">
+        <ReactCurvedText
+          width={200}
+          height={200}
+          cx={100}
+          cy={100}
+          rx={50}
+          ry={50}
+          startOffset={0}
+          reversed={true}
+          text={text}
+          textProps={fontSize ? { style: { fontSize: fontSize } } : null}
+          textPathProps={textPathFill ? { fill: textPathFill } : null}
+          tspanProps={null}
+          ellipseProps={null}
+          svgProps={{ className: "rotating-curved-text" }}
+        />
+      </a>
+      </div>
+      <WorkGallery works={works} />
+      <br />
+      <br />
+      <ButtonArt link="art" label="Art" size="large" />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
